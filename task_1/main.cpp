697060beff0b46ec637e33225531039b1f679852
mandesero <mandesero@gmail.com>
2023-09-13 16:55:57 +0300

    task 1

--- task_1/main.cpp
+++ task_1/main.cpp
@@ -1,14 +1,14 @@
 #include <array>
 #include <iostream>
 
-constexpr int Det(const std::array<std::array<int, 1>, 1> &matrix)
+template <int N>
+constexpr std::array<std::array<int, N - 1>, N - 1> Submatrix(const std::array<std::array<int, N>, N> &matrix, const int row, const int colomn)
 {
-    return matrix[0][0];
-}
-
-constexpr int Det(const std::array<std::array<int, 2>, 2> &matrix)
-{
-    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    std::array<std::array<int, N - 1>, N - 1> sb{};
+    for (int i = 0; i < N - 1; ++i)
+        for (int j = 0; j < N - 1; ++j)
+            (&std::get<0>(((&std::get<0>(sb))[i])))[j] = matrix[i + (i >= row ? 1 : 0)][j + (j >= colomn ? 1 : 0)];
+    return sb;
 }
 
 template <int N>
@@ -17,28 +17,44 @@ constexpr int Det(const std::array<std::array<int, N>, N> &matrix)
     int det = 0;
     for (int i = 0; i < N; ++i)
     {
-        std::array<std::array<int, N - 1>, N - 1> submatrix{};
-        for (int j = 1; j < N; ++j)
-        {
-            int k = 0;
-            for (int l = 0; l < N; ++l)
-            {
-                if (l == i)
-                    continue;
-                (&std::get<0>(((&std::get<0>(submatrix))[j - 1])))[k++] = matrix[j][l];
-            }
-        }
-        det += matrix[0][i] * Det(submatrix) * ((i % 2 == 0) ? 1 : -1);
+        auto sb = Submatrix<N>(matrix, 0, i);
+        det += matrix[0][i] * Det<N - 1>(sb) * ((i % 2 == 0) ? 1 : -1);
     }
     return det;
 }
 
+template <>
+constexpr int Det<1>(const std::array<std::array<int, 1>, 1> &matrix)
+{
+    return matrix[0][0];
+}
+template <>
+constexpr int Det<2>(const std::array<std::array<int, 2>, 2> &matrix)
+{
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+}
+
 int main()
 {
-    constexpr std::array<std::array<int, 3>, 3> matrix = {{{2, 0, 0},
-                                                           {0, 2, 0},
-                                                           {0, 0, 2}}};
-    constexpr int result = Det<3>(matrix);
-    std::cout << result << std::endl;
+    constexpr std::array<std::array<int, 3>, 3> matrix1 = {{{0, 1, 2},
+                                                           {1, 2, 3},
+                                                           {2, 3, 7}}};
+    constexpr int result1 = Det<3>(matrix1);
+    std::cout << result1 << std::endl;
+
+    constexpr std::array<std::array<int, 4>, 4> matrix2 = {{{2, 1, 1, 1},
+                                                           {-3, 2, 1, 1},
+                                                           {3, 3, 2, 1},
+                                                           {1, 2, 3, 4}}};
+    constexpr int result2 = Det<4>(matrix2);
+    std::cout << result2 << std::endl;
+
+    constexpr std::array<std::array<int, 5>, 5> matrix3 = {{{2, 1, 1, 1, 4},
+                                                           {-3, 2, 1, 1, 3},
+                                                           {3, 3, 2, 1, 2},
+                                                           {1, 2, 3, 4, 1},
+                                                           {1, 2, 3, 4, 0}}};
+    constexpr int result3 = Det<5>(matrix3);
+    std::cout << result3 << std::endl;
     return 0;
 }
